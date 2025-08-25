5130badc89320bae87f14621d596319efa258c7f
Hector Lozano <hector@halozano.com>
2025-08-25 17:54:53 -0600

    🏷️ feat: update FavoriteCity schema for optional user association
    - Add optional belongs_to user relationship
    - Update changeset to accept user_id parameter
    - Update unique constraint to be user-specific for authenticated users
    - Keep user_id as optional field for backward compatibility with anonymous favorites

--- lib/clima/favorite_city.ex
+++ lib/clima/favorite_city.ex
@@ -10,14 +10,19 @@ defmodule Clima.FavoriteCity do
     field :lon, :float
     field :openweather_id, :integer
 
+    belongs_to :user, Clima.Accounts.User
+
     timestamps()
   end
 
   @doc false
   def changeset(favorite_city, attrs) do
     favorite_city
-    |> cast(attrs, [:name, :country, :state, :lat, :lon, :openweather_id])
+    |> cast(attrs, [:name, :country, :state, :lat, :lon, :openweather_id, :user_id])
     |> validate_required([:name, :country, :lat, :lon])
-    |> unique_constraint([:lat, :lon])
+    |> unique_constraint([:user_id, :lat, :lon],
+      name: :favorite_cities_user_lat_lon_index,
+      message: "You have already favorited this city"
+    )
   end
 end
